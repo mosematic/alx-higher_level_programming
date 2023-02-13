1368c7ddba736b1fd6d866def365b9d9d61bb889
Mosematic <moselee10@gmail.com>
2023-02-14 01:37:49 +0300

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,27 @@
-ns the JSON string representation of list_dictionaries.
+#!/usr/bin/python3
+"""
+Module that contains the Class Base
+"""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """ Base class of all other classes in this project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Class constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries.
         Args:
             list_dictionaries (list): a list of dictionnary
         """
